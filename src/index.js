c490b58928513855bc733a5362660fed750d1e02
akhilmern <46927802+akhilmern@users.noreply.github.com>
2022-06-10 14:16:35 +0530

    completed to do

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Try from './try';
-import New from './new';
-import New1 from './new1';
-import Calculator from './calculator';
-import Table from './table';
+// import App from './App';
+// import Try from './try';
+// import New from './new';
+// import New1 from './new1';
+// import Calculator from './calculator';
+// import Table from './table';
 import Todo from './todo';
 
 
--- src/todo.js
+++ src/todo.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { useState } from 'react';
 const Todo = () => {
     const[list,setList]=useState("")
+    const[todo,setTodo]=useState([])
 
     const thingsToDo=(e)=>{
         setList(e.target.value)
     }
+    const addButton=()=>{
+        setTodo([...todo,list])
+
+    }
+
+    console.log(todo)
+    console.log(list)
 
 
-    return (  
+    return ( 
+        <div>
         <input value={list} onChange={(e)=>thingsToDo(e)}/>
+        <button onClick={()=>addButton()}>add</button>
+
+        <ol>
+        {todo.map(eachtodo=>{
+            return <li>{eachtodo}</li>
+
+        })}
+        </ol>
+        </div>
     );
 }
  
 export default Todo;
+
